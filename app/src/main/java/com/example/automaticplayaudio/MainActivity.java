4e276af3407438a13343baf572eff400d60d21b5
sofronas <sofronas.konstantinos@gmail.com>
2022-08-10 13:00:58 +0300

    Automatic Play music when android app begin.

---
+++ app/src/main/java/com/example/automaticplayaudio/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.automaticplayaudio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.AudioManager;
+import android.os.Bundle;
+import android.media.MediaPlayer;
+
+import java.sql.SQLOutput;
+
+public class MainActivity extends AppCompatActivity {
+    MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.san);
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                finishAffinity();
+                System.exit(0);
+            }
+        });
+        mediaPlayer.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mediaPlayer.stop();
+        mediaPlayer.release();
+    }
+
+}
